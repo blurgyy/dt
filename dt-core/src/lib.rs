ef7f517c1135b6c57d69115adfe29b977234e4da
Gaoyang Zhang <gy@blurgy.xyz>
2021-09-20 23:18:09 +0800

    Add project initial structure
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

---
+++ dt-core/src/lib.rs
@@ -0,0 +1,4 @@
+
+
+// Author: Blurgy <gy@blurgy.xyz>
+// Date:   Sep 17 2021, 21:32 [CST]
---
+++ dt-core/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Author: Blurgy <gy@blurgy.xyz>
+// Date:   Sep 17 2021, 21:34 [CST]
