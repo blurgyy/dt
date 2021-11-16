3d2cd8c2e60462883166288f9a080cf920c40874
Gaoyang Zhang <gy@blurgy.xyz>
2021-11-16 15:06:01 +0800

    fix: do not create missing staging/target directory during dry run
    Signed-off-by: Gaoyang Zhang <gy@blurgy.xyz>

--- dt-core/src/syncing.rs
+++ dt-core/src/syncing.rs
@@ -568,23 +568,29 @@ fn sync_core(params: SyncingParameters) -> Result<()> {
 
     // First, get target path (without the per-host suffix).
     let tpath = spath.make_target(&hostname_sep, &basedir, &tparent)?;
-    std::fs::create_dir_all(tpath.parent().unwrap_or_else(|| {
-        panic!(
-            "Structrue of target directory could not be created at '{}'",
-            tpath.display(),
-        )
-    }))?;
+    if !dry {
+        std::fs::create_dir_all(tpath.parent().unwrap_or_else(|| {
+            panic!(
+                "Structrue of target directory could not be created at '{}'",
+                tpath.display(),
+            )
+        }))?;
+    }
 
     // Finally, get the staging path with source path (staging path does not
     // have host-specific suffix).
     let staging_path =
         spath.make_target(&hostname_sep, &basedir, &staging)?;
-    std::fs::create_dir_all(staging_path.parent().unwrap_or_else(|| {
-        panic!(
+    if !dry {
+        std::fs::create_dir_all(staging_path.parent().unwrap_or_else(
+            || {
+                panic!(
             "Structrue of staging directory could not be created at '{}'",
             staging_path.display(),
         )
-    }))?;
+            },
+        ))?;
+    }
 
     if spath.is_file() {
         if tpath.is_dir() {
